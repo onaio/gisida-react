00eceadb40cb86618da471701979c62fd54dd8f6
Kipchirchir Sigei <arapgodsmack@gmail.com>
2018-02-21 10:36:04 +0300

    WIP refactor legends
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/lib/components/Legend/Legend.js
+++ src/lib/components/Legend/Legend.js
@@ -1,8 +1,61 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './Legend.scss';
 
-const mapId = '01';
+const mapStateToProps = (state, ownProps) => {
+  return {
+	layerObj: state.MAP.layers[state.MAP.activeLayerId],
+  }
+}
 
-const Legend = () => (<div className={`legend ${mapId}`} />);
+class Legend extends React.Component {
 
-export default Legend;
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  render() {
+	const mapId = '01';
+	const { layerObj } = this.props;
+	if (!layerObj) {
+	  return false;
+	}
+	let shapesArr = [];
+	const circleType = (layerObj && layerObj.credit && layerObj.type === 'circle' && !layerObj.categories.shape && layerObj.visible);
+	const classKeys = ["circle-sm", "circle-md", "circle-lg"];
+
+	for (let c = 0; c < classKeys.length; c+=1) {
+		shapesArr.push((
+			<span
+           className={classKeys[categories]}
+           style={{ background: circleType ? layerObj.categories.color : ' none'}}
+           key={classKeys[c]}
+	       >
+	        </span>
+		));
+	}
+
+    return (
+    	<div>
+        <div className={`legend ${mapId}`}>
+          {circleType ?
+        	<div
+        	  id={`legend-${layerObj.id}-${mapId}`}
+	      	  className="legend-shapes legend-row"
+    	      data-layer={`${layerObj.id}`}
+    	    >
+    	      <b>
+      	        {layerObj.label}
+      	      </b>
+    	      <div className="legend-symbols">
+	           {shapesArr}
+    	      </div>
+    	      <span>{layerObj.credit}</span>
+        	</div> : ''}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(Legend);
