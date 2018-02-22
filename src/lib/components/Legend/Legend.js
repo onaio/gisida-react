cb4979f45b95a72a38bd41e7643b7d5f0a252623
Kipchirchir Sigei <arapgodsmack@gmail.com>
2018-02-22 10:27:01 +0300

    Abstract legend layer type classes
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/lib/components/Legend/Legend.js
+++ src/lib/components/Legend/Legend.js
@@ -4,7 +4,7 @@ import './Legend.scss';
 
 const mapStateToProps = (state, ownProps) => {
   return {
-	layerObj: state.MAP.layers[state.MAP.activeLayerId],
+    layerObj: state.MAP.layers[state.MAP.activeLayerId],
   }
 }
 
@@ -15,43 +15,43 @@ class Legend extends React.Component {
   }
 
   render() {
-	const mapId = '01';
-	const { layerObj } = this.props;
-	if (!layerObj) {
-	  return false;
-	}
-	let shapesArr = [];
-	const circleType = (layerObj && layerObj.credit && layerObj.type === 'circle' && !layerObj.categories.shape && layerObj.visible);
-	const classKeys = ["circle-sm", "circle-md", "circle-lg"];
+    const mapId = '01';
+    const { layerObj } = this.props;
+    if (!layerObj) {
+      return false;
+    }
+    let shapesArr = [];
+    const circleType = (layerObj && layerObj.credit && layerObj.type === 'circle' && !layerObj.categories.shape && layerObj.visible);
+    const classKeys = ["sm", "md", "lg"];
 
-	for (let c = 0; c < classKeys.length; c+=1) {
-		shapesArr.push((
-			<span
-           className={classKeys[categories]}
-           style={{ background: circleType ? layerObj.categories.color : ' none'}}
-           key={classKeys[c]}
-	       >
-	        </span>
-		));
-	}
+    for (let c = 0; c < classKeys.length; c += 1) {
+      shapesArr.push((
+        <span
+          className={`circle-${classKeys[c]}`}
+          style={{ background: circleType ? layerObj.categories.color : ' none' }}
+          key={classKeys[c]}
+        >
+        </span>
+      ));
+    }
 
     return (
-    	<div>
+      <div>
         <div className={`legend ${mapId}`}>
           {circleType ?
-        	<div
-        	  id={`legend-${layerObj.id}-${mapId}`}
-	      	  className="legend-shapes legend-row"
-    	      data-layer={`${layerObj.id}`}
-    	    >
-    	      <b>
-      	        {layerObj.label}
-      	      </b>
-    	      <div className="legend-symbols">
-	           {shapesArr}
-    	      </div>
-    	      <span>{layerObj.credit}</span>
-        	</div> : ''}
+            <div
+              id={`legend-${layerObj.id}-${mapId}`}
+              className="legend-shapes legend-row"
+              data-layer={`${layerObj.id}`}
+            >
+              <b>
+                {layerObj.label}
+              </b>
+              <div className="legend-symbols">
+                {shapesArr}
+              </div>
+              <span>{layerObj.credit}</span>
+            </div> : ''}
         </div>
       </div>
     );
