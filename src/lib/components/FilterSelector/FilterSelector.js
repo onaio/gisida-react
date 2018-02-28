ddab44066224c25588117a9bbee0ee3317de4b2d
Roy Rutto <royrutto@gmail.com>
2018-02-28 13:07:55 +0300

    Fix eslint errors

--- src/lib/components/FilterSelector/FilterSelector.js
+++ src/lib/components/FilterSelector/FilterSelector.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Actions, prepareLayer  } from 'gisida';
+import { prepareLayer  } from 'gisida';
 
 require('./FilterSelector.scss');
 
