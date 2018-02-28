132750e7861e886ca48aa211d53142b84bbd1ee4
Roy Rutto <royrutto@gmail.com>
2018-02-28 10:48:50 +0300

    Add Layer.test.js

--- src/lib/components/Layer/Layer.js
+++ src/lib/components/Layer/Layer.js
@@ -1,34 +1,25 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
 import PropTypes from 'prop-types';
 import { Actions, prepareLayer } from 'gisida'
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-  }
-}
-
-class Layer extends Component {
+export class Layer extends Component {
 
   onLayerToggle = (e, layer) => {
-    const dispatch = this.props.dispatch;
+    // dispatch toggle layer action
+    this.props.dispatch(Actions.toggleLayer(layer.id, e.target.checked));
 
-    // dispatch toggle layer 
-    dispatch(Actions.toggleLayer(layer.id, e.target.checked));
-
-
-    // dispach prepare layer if layer data has not been loaded into props
+    // Prepare layer if layer had not been loaded
     if (!layer.loaded && !layer.isLoading) {
-      prepareLayer(layer, dispatch);
+      prepareLayer(layer, this.props.dispatch);
     }
   }
 
   render() {
-    const layer = this.props.layer;
-
+    const layer = this.props.layer; 
     return (
       <li className="layer">
         <input
+          id={layer.id}  
           type="checkbox"
           data-layer={layer.id}
           onChange={e => this.onLayerToggle(e, layer)}
@@ -43,5 +34,3 @@ class Layer extends Component {
 Layer.propTypes = {
   layer: PropTypes.objectOf(PropTypes.any).isRequired,
 };
-
-export default connect(mapStateToProps)(Layer);
--- test/lib/components/Layer/Layer.test.js
+++ test/lib/components/Layer/Layer.test.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Layer from '../../../../src/lib/components/Layer/Layer'
+import { Layer } from '../../../../src/lib/components/Layer/Layer'
 import layerObj from '../../../fixtures/sample-layer.json';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+
+layerObj.id = 'sample-layer';
+const componentWrapper = shallow(
+  <Layer layer={layerObj} />
+);
 
 describe('Layer', () => {
-  it('Layer renders each layer indicator', () => {
-    const component = renderer.create(
-      <Layer
-        layer={layerObj}
-      />);
-    const json = component.toJSON();
+  it('Layer comonent renderes correctly', () => {
+    const json = toJson(componentWrapper)
     expect(json).toMatchSnapshot();
   });
+
+  it('onLayerToggle is called when layer is checked', () => {
+    const event = {
+      target:
+        { checked: true }
+    }
+    componentWrapper.instance().onLayerToggle = jest.fn();
+    componentWrapper.update();
+    componentWrapper.find(`#${layerObj.id}`).simulate('change', event);
+    expect(componentWrapper.instance().onLayerToggle).toBeCalledWith(event, layerObj);
+
+  })
 });
