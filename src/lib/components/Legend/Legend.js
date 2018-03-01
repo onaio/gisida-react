5be13e147efbae0fc055d3f4001dc84a4d578073
Kipchirchir Sigei <arapgodsmack@gmail.com>
2018-03-01 10:54:32 +0300

    Fix legends for fill layers with break
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/lib/components/Legend/Legend.js
+++ src/lib/components/Legend/Legend.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Actions, generateStops, formatNum, hexToRgbA } from 'gisida';
+import { Actions, formatNum, hexToRgbA } from 'gisida';
 import { buildLayersObj } from '../../utils';
 import Parser from 'html-react-parser';
 import './Legend.scss';
@@ -52,15 +52,13 @@ export class Legend extends React.Component {
       const circleLayerType = (layer && layer.credit && layer.type === 'circle' && !layer.categories.shape && layer.visible);
       const symbolLayer = (layer && layer.credit && layer.categories &&  layer.categories.shape && layer.type !== 'circle'); 
       const fillLayerNoBreaks = (layer && layer.credit && layer.categories && layer.categories.breaks === 'no');
-      const fillLayerWithBreaks = (layer && layer.credit && layer.type !== 'chart');
+      const fillLayerWithBreaks = (layer && layer.credit && layer.type !== 'chart' && layer.type !== 'circle');
       const activeLayerSelected =  this.props.primaryLayer === layer.id ? 'primary' : '';
       const lastSelected = this.props.layersData[this.props.layersData.length - 1].id === layer.id  ? 'primary' : '';
-      const timefield = (layer && layer.aggregate && layer.aggregate.timeseries) ? layer.aggregate.timeseries.field : '';
 
       const boundaries = ["region-boundaries", "district-boundaries"];
       const isBoundaries = boundaries.indexOf(layer.id) !== -1;
 
-      let stops;
       let background = [];
 
       if (layerObj.id === layer.id) {
@@ -132,23 +130,95 @@ export class Legend extends React.Component {
             </div>
           );
         }
-        // else {
-        //   primaryLegend = (
-        //     <div
-        //       id={`legend-${layer.id}-${mapId}`}
-        //       className={`legend-row primary`}
-        //       data-layer={`${layer.id}`}
-        //       key={l}
-        //     >
-        //       <b>
-        //         {layer.label}
-        //       </b>
-        //       <span>
-        //         {Parser(layer.credit)}
-        //       </span>
-        //     </div>
-        //   )
-        // }
+        if (fillLayerWithBreaks && layerObj.stops && layer.stops) {
+          const { stopsData, breaks, colors, Data } = layerObj;
+
+          const dataValues = Data.map(values => values[layer.property]);
+          const colorLegend = [...new Set(stopsData.map(stop => stop[1]))];
+          const legendSuffix = layer.categories.suffix ? layer.categories.suffix : '';
+
+          if (colorLegend.includes('transparent') && !(colors).includes('transparent')) {
+            colors.splice(0, 0, 'transparent');
+            breaks.splice(1, 0, breaks[0]);
+          }
+
+          colorLegend.forEach((color, index) => {
+            const firstVal = breaks[index - 1] !== undefined ? breaks[index - 1] : 0;
+            const lastVal = color === colorLegend[colorLegend.length - 1] || breaks[index] === undefined ? Math.max(...dataValues) : breaks[index];
+            background.push((
+              <li
+                key={index}
+                className={`background-block-${layer.id}-${mapId}`}
+                data-tooltip={`${formatNum(firstVal, 1)}-${formatNum(lastVal, 1)}${legendSuffix}`}
+                style={{ background: hexToRgbA(color, 0.9).toString(), width: (100 / colorLegend.length) + '%' }}
+              >
+              </li>
+            ));
+          });
+
+          primaryLegend = (
+            <div
+            id={`legend-${layer.id}-${mapId}`}
+            className={`legend-row ${activeLayerSelected || lastSelected}`}
+            data-layer={`${layer.id}`}
+            key={l}
+            onClick={(e) => this.onUpdatePrimaryLayer(e)}
+          >
+            <b>
+              {layer.label}
+            </b>
+            <ul
+              className="legend-limit"
+              style={{ padding: '0% 0% 3% 0%' }}
+            >
+              <li
+                id={`first-limit-${layer.id}`}
+                className={`${mapId}`}
+                style={{ position: 'absolute', listStyle: 'none', display: 'inline', left: '3%' }}
+              >
+                {0}
+                {legendSuffix}
+              </li>
+              <li
+                id={`last-limit-${layer.id}`}
+                className={`${mapId}`}
+                style={{ position: 'absolute', listStyle: 'none', display: 'inline', right: '3%' }}
+              >
+                {formatNum(Math.max(...dataValues), 1)}
+                {legendSuffix}
+              </li>
+            </ul>
+            <div
+              className="legend-fill"
+            >
+              <ul
+                id="legend-background"
+              >
+                {background}
+              </ul>
+            </div>
+            <span>{Parser(layer.credit)}</span>
+          </div>
+          );
+
+        }
+        if (!isBoundaries && !symbolLayer && !circleLayerType && !fillLayerNoBreaks && !fillLayerWithBreaks) {
+          primaryLegend = (
+            <div
+              id={`legend-${layer.id}-${mapId}`}
+              className={`legend-row primary`}
+              data-layer={`${layer.id}`}
+              key={l}
+            >
+              <b>
+                {layer.label}
+              </b>
+              <span>
+                {layer.credit ? Parser(layer.credit) : ''}
+              </span>
+            </div>
+          );
+        }
         continue;
       }
       if (circleLayerType) {
@@ -260,91 +330,79 @@ export class Legend extends React.Component {
             </span>
           </div>
         ));
-      } else if (fillLayerWithBreaks) {
-        if (layer.property) {
-          stops = generateStops(layer, timefield)
+      } else if (fillLayerWithBreaks && layer.stops) {
+        const { stopsData, breaks, colors, Data } = layer;
+
+        const dataValues = Data.map(values => values[layer.property]);
+        const colorLegend = [...new Set(stopsData.map(stop => stop[1]))];
+        const legendSuffix = layer.categories.suffix ? layer.categories.suffix : '';
+
+        if (colorLegend.includes('transparent') && !(colors).includes('transparent')) {
+          colors.splice(0, 0, 'transparent');
+          breaks.splice(1, 0, breaks[0]);
         }
 
-        if (stops) {
-          const colorStops = timefield ? stops[0][stops[0].length - 1] : stops[0][0];
-          const radiusStops = stops[1][0];
-          const stopsData = layer.type === 'circle' ? radiusStops : colorStops;
-          const breaks = stops[3];
-          const colors = stops[4];
-          const currPeriod = stops[2][stops[2].length - 1];
-          const currData = layer.source.data.filter(data => data[timefield] === currPeriod);
-          const data = timefield ? currData : layer.source.data;
-          const dataValues = data.map(values => values[layer.property]);
-          const colorLegend = [...new Set(stopsData.map(stop => stop[1]))];
-          const legendSuffix = layer.categories.suffix ? layer.categories.suffix : '';
-
-          if (colorLegend.includes('transparent') && !(colors).includes('transparent')) {
-            colors.splice(0, 0, 'transparent');
-            breaks.splice(1, 0, breaks[0]);
-          }
-
-          colorLegend.forEach((color, index) => {
-            const firstVal = breaks[index - 1] !== undefined ? breaks[index - 1] : 0;
-            const lastVal = color === colorLegend[colorLegend.length - 1] || breaks[index] === undefined ? Math.max(...dataValues) : breaks[index];
-            background.push((
-              <li
-                key={index}
-                className={`background-block-${layer.id}-${mapId}`}
-                data-tooltip={`${formatNum(firstVal, 1)}-${formatNum(lastVal, 1)}${legendSuffix}`}
-                style={{ background: hexToRgbA(color, 0.9).toString(), width: (100 / colorLegend.length) + '%'}}
-              >
-              </li>
-            ));
-          });
-
-          legendItems.unshift((
-            <div
-              id={`legend-${layer.id}-${mapId}`}
-              className={`legend-row ${activeLayerSelected || lastSelected}`}
-              data-layer={`${layer.id}`}
-              key={l}
-              onClick={(e) => this.onUpdatePrimaryLayer(e)}
+        colorLegend.forEach((color, index) => {
+          const firstVal = breaks[index - 1] !== undefined ? breaks[index - 1] : 0;
+          const lastVal = color === colorLegend[colorLegend.length - 1] || breaks[index] === undefined ? Math.max(...dataValues) : breaks[index];
+          background.push((
+            <li
+              key={index}
+              className={`background-block-${layer.id}-${mapId}`}
+              data-tooltip={`${formatNum(firstVal, 1)}-${formatNum(lastVal, 1)}${legendSuffix}`}
+              style={{ background: hexToRgbA(color, 0.9).toString(), width: (100 / colorLegend.length) + '%' }}
             >
-              <b>
-                {layer.label}
-              </b>
-              <ul
-                className="legend-limit"
-                style={{ padding: '0% 0% 3% 0%'}}
-              >
-                <li
-                  id={`first-limit-${layer.id}`}
-                  className={`${mapId}`}
-                  style={{ position: 'absolute', listStyle: 'none', display: 'inline', left: '3%'}}
-                >
-                  {0}
-                  {legendSuffix}
-                </li>
-                <li
-                  id={`last-limit-${layer.id}`}
-                  className={`${mapId}`}
-                  style={{ position: 'absolute', listStyle: 'none', display: 'inline', right: '3%'}}
-                >
-                  {formatNum(Math.max(...dataValues), 1)}
-                  {legendSuffix}
-                </li>
-              </ul>
-              <div
-                className="legend-fill"
-              >
-                <ul
-                  id="legend-background"
-                >
-                  {background}
-                </ul>
-              </div>
-              <span>{Parser(layer.credit)}</span>
-            </div>
+            </li>
           ));
-        }
+        });
+
+        legendItems.unshift((
+          <div
+            id={`legend-${layer.id}-${mapId}`}
+            className={`legend-row ${activeLayerSelected}`}
+            data-layer={`${layer.id}`}
+            key={l}
+            onClick={(e) => this.onUpdatePrimaryLayer(e)}
+          >
+            <b>
+              {layer.label}
+            </b>
+            <ul
+              className="legend-limit"
+              style={{ padding: '0% 0% 3% 0%' }}
+            >
+              <li
+                id={`first-limit-${layer.id}`}
+                className={`${mapId}`}
+                style={{ position: 'absolute', listStyle: 'none', display: 'inline', left: '3%' }}
+              >
+                {0}
+                {legendSuffix}
+              </li>
+              <li
+                id={`last-limit-${layer.id}`}
+                className={`${mapId}`}
+                style={{ position: 'absolute', listStyle: 'none', display: 'inline', right: '3%' }}
+              >
+                {formatNum(Math.max(...dataValues), 1)}
+                {legendSuffix}
+              </li>
+            </ul>
+            <div
+              className="legend-fill"
+            >
+              <ul
+                id="legend-background"
+              >
+                {background}
+              </ul>
+            </div>
+            <span>{Parser(layer.credit)}</span>
+          </div>
+        ));
       }
       else {
-        if (!isBoundaries) {
+        if (!isBoundaries && !symbolLayer && !circleLayerType && !fillLayerNoBreaks && !fillLayerWithBreaks) {
           legendItems.unshift((
             <div
               id={`legend-${layer.id}-${mapId}`}
