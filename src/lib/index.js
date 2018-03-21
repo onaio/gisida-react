a0846d252e7ca55e14960b6ebb7cb08d5a1dcf75
Roy Rutto <royrutto@gmail.com>
2018-03-21 12:32:43 +0300

    add gisida-react-private submodule

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,14 +1,30 @@
+import fs from 'fs';
 import App from './components/App/App';
 import TitleBar from './components/TitleBar/TitleBar';
 import Menu from './components/Menu/Menu';
 import Map from './components/Map/Map';
 import StyleSelector from './components/StyleSelector/StyleSelector';
-import FilterSelector from '../lib/components/FilterSelector/FilterSelector';
 import Legend from './components/Legend/Legend';
-import Filter from './components/Filter/Filter';
 import SummaryChart from './components/Charts/SummaryChart';
-import TimeSeriesSlider from './components/TimeSeriesSlider/TimeSeriesSlider';
 import DetailView from './components/DetailView/DetailView';
+
+// Paths for sub-module components
+const FilterSelectorPath = './submodules/gisida-react-private/src/components/FilterSelector/FilterSelector';
+const TimeSeriesSliderPath = './submodules/gisida-react-private/src/components/TimeSeriesSlider/TimeSeriesSlider';
+const FilterPath = './submodules/gisida-react-private/src/components/Filter/Filter';
+
+// Assign component var if submodule paths found
+let FilterSelector;
+let TimeSeriesSlider;
+let Filter;
+
+if (fs.existsSync(FilterSelectorPath)) {
+  FilterSelector = require(`${FilterSelectorPath}`);
+} else if (fs.existsSync(`${TimeSeriesSliderPath}`)) {
+  TimeSeriesSlider(require(TimeSeriesSliderPath));
+} else if (fs.existsSync(Filter)) {
+  Filter = require(`${FilterPath}`);
+}
 
 export {
   App,
@@ -17,9 +33,10 @@ export {
   Map,
   StyleSelector,
   Legend,
+  SummaryChart,
+  DetailView,
+  // Submodule components
   Filter,
   FilterSelector,
-  SummaryChart,
   TimeSeriesSlider,
-  DetailView,
 }
