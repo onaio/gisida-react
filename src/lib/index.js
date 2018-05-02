44a68d1d57423214bfa25b6d70720f8376aa39f8
cKellyDesign <ckelly@ona.io>
2018-05-02 09:45:57 -0400

    Clean up index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,26 +1,34 @@
+// Gisida Core Map Components
 import App from './components/App/App';
+import Map from './components/Map/Map';
+
+// Gisida Core Interface Components
 import TitleBar from './components/TitleBar/TitleBar';
 import Menu from './components/Menu/Menu';
-import Map from './components/Map/Map';
 import StyleSelector from './components/StyleSelector/StyleSelector';
 import Legend from './components/Legend/Legend';
+import Spinner from './components/Spinner/Spinner';
+
+// Data Exploration Components
 import SummaryChart from './components/Charts/SummaryChart';
 import DetailView from './components/DetailView/DetailView';
 import Filter from './components/Filter/Filter';
 import TimeSeriesSlider from './components/TimeSeriesSlider/TimeSeriesSlider';
 
-import BarChartStacked from './components/Charts/BarChartStacked';
-import Spinner from './components/Spinner/Spinner';
+// Gisida Charts
 import PieChart from './components/Charts/PieChart';
 import LineChart from './components/Charts/LineChart';
+import ColumnChart from './components/Charts/BarChartStacked';
+import BarChartStacked from './components/Charts/BarChartStacked';
 
 export {
   App,
+  Map,
   TitleBar,
   Menu,
-  Map,
   StyleSelector,
   Legend,
+  Spinner,
   SummaryChart,
   DetailView,
   Filter,
@@ -29,5 +37,4 @@ export {
   PieChart,
   ColumnChart,
   BarChartStacked,
-  Spinner,
 }
