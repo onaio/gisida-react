9b1464d6fb1d7754c93d49a71a6a8e3211cc207b
Philipp Seitz <philippseitz2@gmail.com>
2018-06-14 15:54:10 -0400

    Edits
    A few edits but nothing important

--- test/lib/components/ThemeSwitcher/ThemeSwitcher.test.js
+++ test/lib/components/ThemeSwitcher/ThemeSwitcher.test.js
@@ -4,13 +4,13 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 const showFilterPanel = true;
-const componentWrapper = shallow(
+
+describe('ThemeSwitcher', () => {
+  const componentWrapper = shallow(
   <ThemeSwitcher
     showFilterPanel={showFilterPanel}
   />
-);
-
-describe('ThemeSwitcher', () => {
+  );
   it('ThemeSwitcher component renderes correctly', () => {
     const json = toJson(componentWrapper)
     expect(json).toMatchSnapshot();
