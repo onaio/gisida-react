f74abae0037aa42a13f4c1e88c7b7898a615f91f
Philipp Seitz <philippseitz2@gmail.com>
2018-06-20 13:56:23 -0400

    Included function for TimeSeriesSlider
    not sure as to why it's running tests for those that I commented out

--- test/lib/components/FilterSelector/FilterSelector.test.js
+++ test/lib/components/FilterSelector/FilterSelector.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { FilterSelector }  from '../../../../src/lib/components/FilterSelector/FilterSelector.js'
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import layerObj from '../../../fixtures/sample-layer.json';
+//import layerObj from '../../../fixtures/sample-layer.json';
 
-//const layerObj = {'testlayer': 'layer1'};
+const layerObj = {'testlayer': 'layer1'};
 
 const componentWrapper = shallow(
 	<FilterSelector 
