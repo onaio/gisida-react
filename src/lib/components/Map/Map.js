5768c0769aee9dca21aa561176e6212ded821307
cKellyDesign <ckelly@ona.io>
2018-09-26 17:55:20 -0400

    Try to reduce memory leaks with deep object copies

--- src/lib/components/Map/Map.js
+++ src/lib/components/Map/Map.js
@@ -35,7 +35,7 @@ class Map extends Component {
     if (accessToken && mapConfig) {
       mapboxgl.accessToken = accessToken;
       this.map = new mapboxgl.Map(mapConfig);
-      window.maps.push(this.map);
+      // window.maps.push(this.map);
       this.map.controls = new mapboxgl.NavigationControl();
       this.map.addControl(this.map.controls);
   
@@ -275,7 +275,7 @@ class Map extends Component {
       }
     }
     // Assign global variable for debugging purposes.
-    window.GisidaMap = this.map;
+    // window.GisidaMap = this.map;
   }
 
   componentDidUpdate(prevProps, prevState) {
