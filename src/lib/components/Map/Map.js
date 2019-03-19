1b004d400774db56a1b8a0f0d64a24d83a6da813
Kipchirchir Sigei <arapgodsmack@gmail.com>
2019-03-19 18:35:37 +0300

    make originalLayer check more specific
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/lib/components/Map/Map.js
+++ src/lib/components/Map/Map.js
@@ -338,7 +338,9 @@ class Map extends Component {
             this.map.removeSource(layer.id);
             // 2) dispatch action to set reloadLayerId to null
             this.props.dispatch(Actions.layerReloaded(mapId));
-            const originalLayer = nextProps.FILTER[layer.id].isClear ? nextProps.MAP.oldLayerObjs[layer.id] : layer;
+            const originalLayer = nextProps.FILTER &&
+              nextProps.FILTER[layer.id] &&
+              nextProps.FILTER[layer.id].isClear ? nextProps.MAP.oldLayerObjs[layer.id] : layer;
             prepareLayer(mapId, originalLayer, this.props.dispatch, filterOptions, doUpdateTsLayer);
           }
           // Change visibility if layer is already on map
