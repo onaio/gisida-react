ee7381009ede5f0dae3924ed5cdd91f7f5ef1037
Kipchirchir Sigei <arapgodsmack@gmail.com>
2019-04-12 17:38:38 +0300

    extract oauth token and set Custom-Api-Token Header for superset
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/lib/components/App/App.js
+++ src/lib/components/App/App.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
+import superset from '@onaio/superset-connector';
 import './App.scss';
 
+const { authZ, deAuthZ } = superset;
+
+const getParameterByName = (name) => {
+  var match = RegExp('[#&]' + name + '=([^&]*)').exec(window.location.hash);
+  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+const getAccessToken = () =>  {
+  return getParameterByName('access_token');
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    const accessToken = localStorage.getItem('access_token') || getAccessToken();
+    if (!localStorage.getItem('access_token') && accessToken) {
+      localStorage.setItem('access_token', accessToken);
+      window.history.replaceState('asdf', 'Superset', '/');
+    }
+    this.state = {
+      accessToken,
+    };
+  }
+  componentWillMount() {
+    const { accessToken } = this.state;
+    authZ({
+      token: accessToken,
+      base: 'https://discover.ona.io/'
+    }, (res) => console.log(res.status))
+  }
   render() {
     return (
       <div className="app">
