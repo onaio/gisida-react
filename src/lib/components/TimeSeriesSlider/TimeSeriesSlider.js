34d41cf1139e6102b7b56561f1da47ea23a8ec51
Kipchirchir Sigei <arapgodsmack@gmail.com>
2019-05-15 14:42:15 +0300

    block stops generation for non property driven layers
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- src/lib/components/TimeSeriesSlider/TimeSeriesSlider.js
+++ src/lib/components/TimeSeriesSlider/TimeSeriesSlider.js
@@ -76,6 +76,7 @@ class TimeSeriesSlider extends React.Component {
               temporalIndex,
               data: periodData[period[temporalIndex]].data,
               adminFilter: periodData[period[temporalIndex]].adminFilter && [...periodData[period[temporalIndex]].adminFilter],
+              tsFilter: periodData[period[temporalIndex]].tsFilter && [...periodData[period[temporalIndex]].tsFilter],
             },
           );
         }
@@ -85,21 +86,18 @@ class TimeSeriesSlider extends React.Component {
       field
     } = sliderLayerObj.layerObj.aggregate.timeseries;
     sliderLayerObj.data = sliderLayerObj.periodData[sliderLayerObj.period[nextIndex]].data;
-    const activeStops = generateStops(sliderLayerObj, field, this.props.dispatch, nextIndex);
+    const { layerObj } = sliderLayerObj;
+    if (layerObj.type !== 'chart' && layerObj.property) {
+      const activeStops = generateStops(sliderLayerObj, field, this.props.dispatch, nextIndex);
 
-    const {
-      primaryLayer
-    } = this.props;
-    const activeLayer = layers.find(l => l.id === sliderLayerObj.layerId);
-    if (activeLayer.type !== 'chart') {
+      const { primaryLayer } = this.props;
+
       if (this.props.layers[primaryLayer].layers) {
         this.props.layers[primaryLayer].layers.map(i =>
           nextTimeseries[i].newBreaks = activeStops[3]);
         this.props.layers[primaryLayer].layers.map(i =>
           nextTimeseries[i].newColors = [...new Set(sliderLayerObj.colorStops[nextIndex].map(d =>
             d[1]))]);
-
-        this.props.dispatch(Actions.updateTimeseries(this.props.mapId, nextTimeseries, this.props.timeLayer));
       } else {
         nextTimeseries[sliderLayerObj.layerId].newBreaks = activeStops[3];
         nextTimeseries[sliderLayerObj.layerId].newColors = [...new Set(sliderLayerObj.colorStops[nextIndex].map(d => d[1]))];
