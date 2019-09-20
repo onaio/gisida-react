98fe52f2080fb4d44d0e1ba551e15e8b2d587ceb
kahama <phares663@gmail.com>
2019-09-20 15:00:19 +0300

    Adding folders for various tests

---
+++ test/fixtures/fixtures.js
@@ -0,0 +1,20 @@
+export const detailView = {
+    "UID": "Plots",
+    "title": "Plots",
+    "basicInfo": [{
+        "value": {
+            "prop": [
+                "longitude",
+                "latitude"
+            ],
+            "join": ", "
+        },
+        "icon": "screenshot",
+        "alt": "GPS coordinates"
+    }],
+    "parsedBasicInfo": [{
+        "value": "98.406174, 3.82129",
+        "icon": "glyphicon glyphicon-screenshot",
+        "alt": "GPS coordinates"
+    }]
+}
