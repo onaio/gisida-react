56db9e82e5b3e76038c4f5bab9f85ee942ec0a48
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-01-30 14:57:42 +0300

    Solve bug LAYERS of undefined
    The bug occurs when you open a menu category

--- src/lib/components/Layers/Layers.js
+++ src/lib/components/Layers/Layers.js
@@ -7,13 +7,13 @@ export class Layers extends Component {
     super(props);
     const groups = {};
     if (this.props.layers) {
-      this.props.layers.forEach((layer) => {
+      this.props.layers.forEach(layer => {
         if (!layer.id) {
-          Object.keys(layer).forEach((l) => {
+          Object.keys(layer).forEach(l => {
             groups[l] = { isOpen: false };
           });
         }
-      })
+      });
     }
     this.state = groups;
   }
@@ -33,10 +33,12 @@ export class Layers extends Component {
     let layerItem = [];
     const subLayerIds = [];
     const ifPermissionDenied = () => {
-      return layers.length > 0 ? 
-      (<p>You don't have permision to view this category</p>) :
-       (<p>{noLayerText ? noLayerText : 'No layers available'}</p>);
-    }
+      return layers.length > 0 ? (
+        <p>You don't have permision to view this category</p>
+      ) : (
+        <p>{noLayerText ? noLayerText : 'No layers available'}</p>
+      );
+    };
 
     if (!preparedLayers) {
       return false;
@@ -53,17 +55,13 @@ export class Layers extends Component {
       }
     }
 
-    layers.forEach((layer) => {
-      if ((!currentRegion
-        || (layer.region
-          && layer.region === currentRegion))
-        && !subLayerIds.includes(layer.id)) {
-        if (layer.id && !auth) {
-          layerItem.push((<Layer
-            key={layer.id}
-            mapId={mapId}
-            layer={layer}
-          />))
+    layers.forEach(layer => {
+      if (
+        (!currentRegion || (layer.region && layer.region === currentRegion)) &&
+        !subLayerIds.includes(layer.id)
+      ) {
+        if (layer.id && (!auth || !auth.authConfigs)) {
+          layerItem.push(<Layer key={layer.id} mapId={mapId} layer={layer} />);
         } else if (layer.id && auth) {
           const { authConfigs, userInfo } = auth;
           let activeId = layer.id;
@@ -74,50 +72,48 @@ export class Layers extends Component {
             activeId = activeId.split('.')[0];
           }
           // this is a temporary fix
-          const LocalAuthConfig = JSON.parse(localStorage.getItem('authConfig'))
+          const LocalAuthConfig = JSON.parse(localStorage.getItem('authConfig'));
           users = authConfigs.LAYERS && authConfigs.LAYERS[activeId]; // list of users with access to the layer
           authConfigs.LAYERS = authConfigs.LAYERS || LocalAuthConfig.LAYERS;
           // users = authConfigs.LAYERS[activeId]; // list of users with access to the layer
           // check if logged in user exists in the list of users
           // who have access to the layer
-          if ((users
-            && userInfo
-            && users.includes(userInfo.username))
-            || (authConfigs.LAYERS
-              && authConfigs.LAYERS.ALL
-              && authConfigs.LAYERS.ALL.includes(userInfo.username))) {
-            layerItem.push((<Layer
-              key={layer.id}
-              mapId={mapId}
-              layer={layer}
-            />))
+          if (
+            (users && userInfo && users.includes(userInfo.username)) ||
+            (authConfigs.LAYERS &&
+              authConfigs.LAYERS.ALL &&
+              authConfigs.LAYERS.ALL.includes(userInfo.username))
+          ) {
+            layerItem.push(<Layer key={layer.id} mapId={mapId} layer={layer} />);
           }
         } else {
           Object.keys(layer).forEach((d, i) => {
             layerItem = layerItem.concat([
-              (<li>
+              <li>
                 <a
                   key={`${d}-${i}-link`}
                   className="sub-category"
-                  onClick={(e) => this.toggleSubMenu(e, d)}
+                  onClick={e => this.toggleSubMenu(e, d)}
                 >
                   {d}
                   <span
-                    className={`category glyphicon glyphicon-chevron-${this.state[d].isOpen ? 'down' : 'right'}`}
+                    className={`category glyphicon glyphicon-chevron-${
+                      this.state[d].isOpen ? 'down' : 'right'
+                    }`}
                   />
                 </a>
-              </li>),
-              (this.state[d].isOpen ?
-                  <Layers
-                    key={`${d}-${i}`}
-                    mapId={mapId}
-                    layers={layer[d].layers}
-                    currentRegion={currentRegion}
-                    preparedLayers={preparedLayers}
-                    auth={this.props.auth}
-                    noLayerText={noLayerText}
-                  />
-              : null)
+              </li>,
+              this.state[d].isOpen ? (
+                <Layers
+                  key={`${d}-${i}`}
+                  mapId={mapId}
+                  layers={layer[d].layers}
+                  currentRegion={currentRegion}
+                  preparedLayers={preparedLayers}
+                  auth={this.props.auth}
+                  noLayerText={noLayerText}
+                />
+              ) : null,
             ]);
           });
         }
@@ -125,11 +121,7 @@ export class Layers extends Component {
       return null;
     });
 
-    return (
-      <ul className="layers">
-        { layerItem.length > 0 ? layerItem :  ifPermissionDenied() }
-      </ul>
-    );
+    return <ul className="layers">{layerItem.length > 0 ? layerItem : ifPermissionDenied()}</ul>;
   }
 }
 
