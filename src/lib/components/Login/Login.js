d5a307107e1bc936a84ce45dc8521881caf7928f
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2020-02-06 11:49:00 +0300

    Gate keeper integration

--- src/lib/components/Login/Login.js
+++ src/lib/components/Login/Login.js
@@ -1,119 +1,86 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Cookie from 'js-cookie';
-import { ONA, SupAuth } from 'gisida';
 import './Login.scss';
-import { Router } from '../../routes/router';
+import { useOAuthLogin } from '../../../../node_modules/@onaio/gatekeeper/dist/components/login.js';
+import { AuthorizationGrantType } from '@onaio/gatekeeper';
 
-const { defaultUnSupAuthZ: deAuthZ } = SupAuth;
-
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   const { APP } = state;
   return {
-    appPassword: APP.password,
-    global: state,
     appIcon: APP.appIcon,
-    loginIcon: APP.loginIcon,
-    publicUsername: APP.publicUsername,
-    publicPassword: APP.publicPassword
+    loginIcon: APP.appLoginIcon,
+    publicUsername: APP.authPublicUsername,
+    publicPassword: APP.authPublicPassword,
+    clientID: APP.authClientID,
+    redirectUri: APP.authRedirectUri,
   };
-};
-
-export const isLoggedIn = function() {
-  return Cookie.get('dsauth') === 'true';
-};
-
-export const logOut = (e) => {
-  e.preventDefault();
-  deAuthZ();
-  Cookie.set('dsauth', false);
-  Router.history.push('/login');
 };
 
 class Login extends Component {
   constructor(props) {
     super(props);
-    const clientID = 'CdJqBZYRVrbpnAu4JoYYFXFPQJa3xWi25oDPqnRY';
-    const redirectURL = location.protocol + '//' + location.host + '/callback';
-    const apiBase = props.global.AUTH && props.global.AUTH.apiBase;
     this.state = {
-      loginError: null,
-      clientID,
-      redirectURL,
-      oauthURL: ONA.Oauth2.getOauthURL(clientID, redirectURL, apiBase),
-      redirect: false,
+      authorizationUris: {},
     };
-    this.renderRedirect = this.renderRedirect.bind(this);
   }
-
-  renderRedirect = () => {
-    if (isLoggedIn()) {
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.clientID !== this.props.clientID ||
+      prevProps.redirectUri !== this.props.redirectUri
+    ) {
+      const providers = {
+        onadata: {
+          accessTokenUri: '',
+          authorizationUri: 'https://api.ona.io/o/authorize/',
+          clientId: this.props.clientID,
+          redirectUri: this.props.redirectUri,
+          scopes: ['read', 'write'],
+          state: 'abc',
+          userUri: 'https://api.ona.io/user',
+        },
+      };
+      const options = {
+        providers,
+        authorizationGrantType: AuthorizationGrantType.IMPLICIT,
+      };
+      const authorizationUris = useOAuthLogin(options);
       this.setState({
-        redirect: true,
+        authorizationUris,
       });
-      return Router.Redirect('/');
-    } else {
-      this.setState({
-        redirect: false,
-      });
-    }
-  };
-
-  componentWillReceiveProps(nextProps) {
-    const { clientID, redirectURL } = this.state;
-    const apiBase = nextProps.global.AUTH && nextProps.global.AUTH.apiBase;
-    this.setState({
-      oauthURL: ONA.Oauth2.getOauthURL(clientID, redirectURL, apiBase),
-    });
-  }
-
-  handleLogin(password) {
-    const { appPassword } = this.props;
-    if (appPassword.includes(password)) {
-      Cookie.set('dsauth', true);
-      Router.history.push('/');
-    } else {
-      this.setState({ loginError: true });
     }
   }
 
-  oauth2URL(e) {
-    e.preventDefault();
-    Cookie.set('dsauth', true);
-    window.location.href = this.state.oauthURL;
+  getProviderKey() {
+    return Object.keys(this.state.authorizationUris).filter(key => key === 'onadata')[0];
   }
 
   render() {
     const { loginIcon, appIcon, publicPassword, publicUsername } = this.props;
     const loginImage = {
-      background: `url(${loginIcon || appIcon}) no-repeat center center`
+      background: `url(${loginIcon || appIcon}) no-repeat center center`,
     };
-    return this.state.redirect ? (
-      this.renderRedirect()
-    ) : (
+    const providerKey = this.getProviderKey();
+
+    return (
       <div className="login">
         <div>
           <form className="login-form">
             <div className="form-group">
               <div className="brand-login" style={loginImage}></div>
-              {this.state.loginError === true ? (
-                <div className="alert alert-danger">Incorrect password.</div>
-              ) : null}
-              { publicPassword && publicUsername ? (
+              {publicPassword && publicUsername ? (
                 <div>
                   <small>Username: {publicUsername}</small>
                   <br />
                   <small>Password: {publicPassword}</small>
                 </div>
-              ) : null
-              }
+              ) : null}
             </div>
-            <button
+            <a
               className="btn btn-default center-block btn-block"
-              onClick={(e) => this.oauth2URL(e)}
+              href={this.state.authorizationUris[providerKey]}
             >
-              Log In
-            </button>
+              Login
+            </a>
           </form>
         </div>
       </div>
