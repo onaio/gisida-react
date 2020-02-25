5074af919bbd82cbe018607c7cdf30dd9cc1ac84
ciremusyoka <wambua.eric01@gmail.com>
2020-02-25 12:47:55 +0300

    Clear user input on cancel button click

--- src/lib/components/Searchbar/SearchBar.jsx
+++ src/lib/components/Searchbar/SearchBar.jsx
@@ -20,18 +20,25 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
+      inputText: '',
     }
     this.handleCancel = this.handleCancel.bind(this);
+    this.handleSearchInput = this.handleSearchInput.bind(this);
+  }
+
+  handleSearchInput(e) {
+    this.setState({inputText: e.target.value})
+    this.props.handleSearch(e);
   }
 
   handleCancel(e) {
     e.preventDefault();
+    this.setState({inputText: "", })
     this.props.handleSearchClick(e, true)
   }
 
   render() {
-    const { appColor, searchBarColor, handleSearch, searching, handleSearchClick } = this.props;
+    const { appColor, searchBarColor, searching, handleSearchClick } = this.props;
     const searchBtn = {
       border: `1px solid ${ searchBarColor || appColor || '#00B4CC'}`,
       background: `${ searchBarColor || appColor || '#00B4CC'}`,
@@ -42,8 +49,8 @@ class SearchBar extends Component {
     return (
       <div className="search-wrapper">
       <div className="search">
-          <input type="text" className="searchTerm" style={serchtearm} placeholder="Search..." 
-            onChange={ handleSearch }
+          <input type="text" className="searchTerm" value={this.state.inputText} style={serchtearm} placeholder="Search..." 
+            onChange={ this.handleSearchInput }
           />
           { searching ?
             <button type="button" className="searchButton" style={searchBtn}
