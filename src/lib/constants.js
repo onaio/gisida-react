000f8a119b2acc8277b4a5abd0f0e3ae640e4b90
kahummer <pkahama@ona.io>
2020-03-01 09:48:19 +0300

    Add constants file

---
+++ src/lib/constants.js
@@ -0,0 +1,5 @@
+export const NUTRITION_SITES_LIVE = 'nutrition-sites-live';
+export const NUTRITION_SITES_LIVE_HIGHLIGHT = 'nutrition-sites-live-highlight';
+export const FACILITY_ID = 'facility_id';
+export const ICON_OPACITY = 'icon-opacity';
+export const ALL = 'all';
