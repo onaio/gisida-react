ae516bd07a57bc55caf19a22ea7fbad0c6a65813
ciremusyoka <wambua.eric01@gmail.com>
2020-03-24 17:38:49 +0300

    Remove console

--- src/lib/components/TimeSeriesSlider/TimeSeriesSlider.js
+++ src/lib/components/TimeSeriesSlider/TimeSeriesSlider.js
@@ -83,7 +83,6 @@ class TimeSeriesSlider extends React.Component {
       field
     } = sliderLayerObj.layerObj.aggregate.timeseries;
     sliderLayerObj.data = sliderLayerObj.periodData[sliderLayerObj.period[nextIndex]].data;
-    console.log(field)
     const activeStops = generateStops(sliderLayerObj, field, this.props.dispatch, nextIndex);
 
     const {
