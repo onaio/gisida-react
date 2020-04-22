e80c2b4679c5e3b5c9310f6ff837f87f0d292573
ciremusyoka <wambua.eric01@gmail.com>
2020-04-22 10:32:32 +0300

    Fix TimeSeriesSlider tests tabbing

--- test/lib/components/TimeSeriesSlider/TimeSeriesSlider.test.js
+++ test/lib/components/TimeSeriesSlider/TimeSeriesSlider.test.js
@@ -1,85 +1,77 @@
 import React from 'react';
-import TimeSeriesSlider from '../../../../src/lib/components/TimeSeriesSlider/TimeSeriesSlider.js'
+import TimeSeriesSlider from '../../../../src/lib/components/TimeSeriesSlider/TimeSeriesSlider.js';
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import configureStore from 'redux-mock-store';
 import periodData from '../../../fixtures/periodData.json';
 import { layer1 } from '../../../fixtures/defaultLayers';
-import sampleLayer from '../../../fixtures/sample-layer.json'
-var gisida = require('gisida')
+import sampleLayer from '../../../fixtures/sample-layer.json';
+var gisida = require('gisida');
 
 sampleLayer['id'] = 'sample-layer';
 sampleLayer['visible'] = false;
 layer1['visible'] = true;
 
 const initialState = {
-	"map-1": {
-		primaryLayer: "education",
-		showFilterPanel: false,
-		layers: {"sample-layer": {...sampleLayer}, "education": {...layer1}},
-		timeseries: {}
-	},
-}
-
-const newState = { "education": {
-	...periodData,
-	...layer1,
-  layerObj: layer1,
-  layerId: 'education',
-	colorStops: [[1], [1,2,3,4]]
-	} 
+  'map-1': {
+    primaryLayer: 'education',
+    showFilterPanel: false,
+    layers: { 'sample-layer': { ...sampleLayer }, education: { ...layer1 } },
+    timeseries: {},
+  },
 };
 
-const mockStore = configureStore()
+const newState = {
+  education: {
+    ...periodData,
+    ...layer1,
+    layerObj: layer1,
+    layerId: 'education',
+    colorStops: [[1], [1, 2, 3, 4]],
+  },
+};
+
+const mockStore = configureStore();
 const store = mockStore(initialState);
 
 const updateTimeseriesState = jest.fn();
 
 describe('TimeSeriesSlider', () => {
-
   it('renders without crashing', () => {
     shallow(
-      <TimeSeriesSlider 
-        store={store}
-        mapId='map-1'
-        updateTimeseriesState={updateTimeseriesState}
-      />
-    )
-  })
+      <TimeSeriesSlider store={store} mapId="map-1" updateTimeseriesState={updateTimeseriesState} />
+    );
+  });
 
-	it('TimeSeriesSlider component renders correctly', () => {
+  it('TimeSeriesSlider component renders correctly', () => {
     const wrapper = mount(
-      <TimeSeriesSlider 
-        store={store}
-        mapId='map-1'
-        updateTimeseriesState={updateTimeseriesState}
-      />
+      <TimeSeriesSlider store={store} mapId="map-1" updateTimeseriesState={updateTimeseriesState} />
     );
 
     expect(toJson(wrapper)).toMatchSnapshot();
     wrapper.unmount();
-	})
+  });
 
-	it('should update correctly on input change', () => {
-    gisida.generateStops = jest.fn().mockReturnValue([1,2,3,4]);
+  it('should update correctly on input change', () => {
+    gisida.generateStops = jest.fn().mockReturnValue([1, 2, 3, 4]);
     const updateTimeseriesSpy = jest.spyOn(gisida.Actions, 'updateTimeseries');
     const wrapper = mount(
-      <TimeSeriesSlider 
-        store={store}
-        mapId='map-1'
-        updateTimeseriesState={updateTimeseriesState}
-      />
+      <TimeSeriesSlider store={store} mapId="map-1" updateTimeseriesState={updateTimeseriesState} />
     );
-    // update props:  
-		initialState["map-1"].timeseries = { ...newState };
-    wrapper.setProps({...initialState});
-    wrapper.find('input').simulate('change', {target: {value: 1}});
-    
+    // update props:
+    initialState['map-1'].timeseries = { ...newState };
+    wrapper.setProps({ ...initialState });
+    wrapper.find('input').simulate('change', { target: { value: 1 } });
+
     const props = wrapper.props();
 
     expect(props['map-1'].timeseries).toEqual(newState);
     expect(updateTimeseriesSpy).toHaveBeenCalledTimes(1);
-    expect(gisida.generateStops).toHaveBeenCalledWith(newState['education'], 'period', store.dispatch, 1);
-	})
-
+    expect(gisida.generateStops).toHaveBeenCalledWith(
+      newState['education'],
+      'period',
+      store.dispatch,
+      1
+    );
+  });
 });
