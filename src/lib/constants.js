32ffc7f207cb8838c9940a434fc1d3cebcad8a58
Kahama <kahama94@gmail.com>
2020-04-26 17:16:32 +0300

    Update constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,26 @@
 export const ICON_OPACITY = 'icon-opacity';
 export const ALL = 'all';
 export const HIGHLIGHT_FILTER_PROPERTY = 'highlight-filter-property';
-export const HIGHLIGHT = '-highlight';
+export const DASH_HIGHLIGHT = '-highlight';
+export const HIGHLIGHT = 'highlight';
+export const MAP_1 = 'map-1';
+export const ACTIVE_LAYER_SUPERSET_LINK = 'activeLayerSupersetLink';
+export const CHART = 'chart';
+export const METRIC = 'metric';
+export const DETAIL_VIEW = 'detail-view';
+export const POINTER = 'pointer';
+export const VISIBILITY = 'visibility';
+export const VISIBLE = 'visible'
+export const NONE = 'none';
+export const HIGHLIGHT_LAYOUT = 'highlight-layout';
+export const HIGHLIGHT_PAINT = 'highlight-paint';
+export const UNDERSCORE_CONTAINER = '_container';
+export const CIRCLE = 'circle';
+export const CIRCLE_RADIUS = 'circle-radius';
+export const CATEGORICAL = 'categorical';
+export const FILL_COLOR = 'fill-color';
+export const VECTOR_PROP = 'vector-prop';
+export const CIRCLE_COLOR = 'circle-color';
+export const CIRCLE_STROKE_WIDTH = 'circle-stroke-width';
+
+
