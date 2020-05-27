944c8c4b53b2012c1b2d8424525d9e894843386a
ciremusyoka <wambua.eric01@gmail.com>
2020-05-27 11:37:35 +0300

    Fix failling tests after merging branch 'staging'

--- test/lib/components/TitleBar/TitleBar.test.js
+++ test/lib/components/TitleBar/TitleBar.test.js
@@ -4,8 +4,9 @@ import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import configureStore from 'redux-mock-store';
 
+const appNameDesc = 'Test application';
 const initialState = {
-  APP: { loaded: true },
+  APP: { loaded: true, appNameDesc },
 };
 
 const mockStore = configureStore();
@@ -19,5 +20,6 @@ describe('TitleBar', () => {
   it('TitleBar component renders correctly', () => {
     const wrapper = mount(<TitleBar store={store} />);
     expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.find('.brand-title').text()).toEqual(`   ${appNameDesc}`);
   });
 });
