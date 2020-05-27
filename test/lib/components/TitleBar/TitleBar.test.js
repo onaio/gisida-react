0bbb11e05170f29840b22f11a39b64bff1026a4d
ciremusyoka <wambua.eric01@gmail.com>
2020-05-27 10:44:26 +0300

    Fix tabbing

--- test/lib/components/TitleBar/TitleBar.test.js
+++ test/lib/components/TitleBar/TitleBar.test.js
@@ -1,31 +1,23 @@
 import React from 'react';
-import TitleBar from '../../../../src/lib/components/TitleBar/TitleBar.js'
+import TitleBar from '../../../../src/lib/components/TitleBar/TitleBar.js';
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import configureStore from 'redux-mock-store';
 
 const initialState = {
-	APP: { loaded: true } 
-}
+  APP: { loaded: true },
+};
 
-const mockStore = configureStore()
+const mockStore = configureStore();
 const store = mockStore(initialState);
 
 describe('TitleBar', () => {
-
-	it('renders without crashing', () => {
-    
-    shallow( 
-      <TitleBar  store={store} /> 
-    );
-
-  })
-  
-  it('TitleBar component renders correctly', () => {
-    const wrapper = mount(
-      <TitleBar  store={store} /> 
-    );
-		expect(toJson(wrapper)).toMatchSnapshot();
+  it('renders without crashing', () => {
+    shallow(<TitleBar store={store} />);
   });
-  
+
+  it('TitleBar component renders correctly', () => {
+    const wrapper = mount(<TitleBar store={store} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 });
