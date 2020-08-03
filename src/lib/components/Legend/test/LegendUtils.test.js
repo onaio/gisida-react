af0b9acab3afc977a8b5b9822f559d6c00dba014
Kahama <kahama94@gmail.com>
2020-08-03 20:04:29 +0300

    Update LegendUtils test

--- src/lib/components/Legend/test/LegendUtils.test.js
+++ src/lib/components/Legend/test/LegendUtils.test.js
@@ -2,34 +2,45 @@ import React from 'react';
 import { buildDescription, buildHyperLink, combinedLinks } from '../Utils';
 
 describe('Legend Utils', () => {
-    const layerObj = { legendDescription: 'layer desc', legendHyperlink: 'ona.io', legendHyperlinkText: "Google doc Report" };
-    const legendDescription = (
-        <div className="description">
-            <span className="glyphicon glyphicon-info-sign" />
-            <p>layer desc</p>
-        </div>
-    );
-    const combinedLinksResult = (<div className="combinedLinks">
-        <div className="description">
-            <span className="glyphicon glyphicon-info-sign" />
-            <p>layer desc</p></div>&nbsp;&nbsp;
-        <a href="ona.io"><h5>Google doc Report</h5>
-            <span className="glyphicon glyphicon-file" />
-        </a>
-    </div>);
-    const legendHyperlink = <a href="ona.io">
-        <h5>Google doc Report</h5>
-        <span className="glyphicon glyphicon-file" />
-    </a>;
+  const layerObj = {
+    legendDescription: 'layer desc',
+    legendHyperlink: 'ona.io',
+    legendHyperlinkText: 'Google doc Report',
+  };
+  const legendDescription = (
+    <div className="description">
+      <span className="glyphicon glyphicon-info-sign" />
+      <p>layer desc</p>
+    </div>
+  );
+  const combinedLinksResult = (
+    <div className="combinedLinks">
+      <div className="description">
+        <span className="glyphicon glyphicon-info-sign" />
+        <p>layer desc</p>
+      </div>
+        
+      <a data-link="resourceLink" href="ona.io" target="_blank">
+        <h5 data-link="resourceLink">Google doc Report</h5>
+        <span className="glyphicon glyphicon-file" data-link="resourceLink" />
+      </a>
+    </div>
+  );
+  const legendHyperlink = (
+    <a data-link="resourceLink" href="ona.io" target="_blank">
+      <h5 data-link="resourceLink">Google doc Report</h5>
+      <span className="glyphicon glyphicon-file" data-link="resourceLink" />
+    </a>
+  );
 
-    it('builds legend description correctly', () => {
-        expect(buildDescription(layerObj)).toEqual(legendDescription);
-    });
+  it('builds legend description correctly', () => {
+    expect(buildDescription(layerObj)).toEqual(legendDescription);
+  });
 
-    it('builds legend description correctly', () => {
-        expect(buildHyperLink(layerObj)).toEqual(legendHyperlink);
-    });
-    it('builds legend description correctly', () => {
-        expect(combinedLinks(legendDescription, legendHyperlink)).toEqual(combinedLinksResult);
-    });
+  it('builds legend description correctly', () => {
+    expect(buildHyperLink(layerObj)).toEqual(legendHyperlink);
+  });
+  it('builds legend description correctly', () => {
+    expect(combinedLinks(legendDescription, legendHyperlink)).toEqual(combinedLinksResult);
+  });
 });
