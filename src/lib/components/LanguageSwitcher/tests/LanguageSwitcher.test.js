9004109acb2f34a0b4681959f317fc05b8196db2
Kahummer <pkahama@ona.io>
2021-04-23 23:31:24 +0300

    Update snapshot

--- src/lib/components/LanguageSwitcher/tests/LanguageSwitcher.test.js
+++ src/lib/components/LanguageSwitcher/tests/LanguageSwitcher.test.js
@@ -11,22 +11,40 @@ describe('LanguageSwitcher', () => {
         { label: 'English', param: 'en' },
         { label: 'ជនជាតិខ្មែរ', param: 'khmer' },
       ],
+      languageSwitcherConfigs: {
+        English: {
+          param: 'en',
+        },
+        ជនជាតិខ្មែរ: {
+          param: 'khmer',
+        },
+      },
+      languageTranslations: {
+        khmer: {
+          'Indicator 2.2 RCFs ': 'ជនជាតិខ្មែរ ជនជាតិខ្មែរ',
+        },
+      },
+      mapLanguageTranslation: false,
     },
   };
   const mockStore = configureStore();
 
   it('renders without crashing', () => {
     const store = mockStore(initialState);
-    shallow(<Provider store={store}><LanguageSwitcher /></Provider>);
+    shallow(
+      <Provider store={store}>
+        <LanguageSwitcher />
+      </Provider>
+    );
   });
 
   it('component renders correctly', () => {
     const store = mockStore(initialState);
     const wrapper = mount(
-        <Provider store={store}>
-          <LanguageSwitcher />
-        </Provider>
-      );
+      <Provider store={store}>
+        <LanguageSwitcher />
+      </Provider>
+    );
     expect(wrapper.text()).toMatchSnapshot('Text snapshot');
     expect(wrapper.html()).toMatchSnapshot('Elements snapshot');
   });
